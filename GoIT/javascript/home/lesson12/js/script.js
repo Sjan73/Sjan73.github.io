faeb71b51d8334a1a044adaef913f63658337a8f
Andrii Palas <sajn73@gmail.com>
2016-01-20 22:08:46 +0200

    JS Template

--- GoIT/javascript/home/lesson12/js/script.js
+++ GoIT/javascript/home/lesson12/js/script.js
@@ -5,11 +5,12 @@ $(function() {
 		url: 'andriipalas2.jpg',
 		mob: 'Моб.: 063-67-925-73',
 		email: 'sajn73@gmail.com',
-		facebook: 'Andrii Palas'
+		facebook: 'Andrii Palas',
+		divClass: 'header',
+		divClass_2: 'border_line'
 	};
 
 	var content = tmpl(html, data);
 
-	console.log(content);
 	$('body').append(content);
 });
