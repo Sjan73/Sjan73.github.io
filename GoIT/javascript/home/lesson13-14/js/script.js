5fcfd7154f82b646753b6e204bb3ec46bfbed7b0
Andrii Palas <sajn73@gmail.com>
2016-02-02 21:48:06 +0200

    quiz js

--- GoIT/javascript/home/lesson13-14/js/script.js
+++ GoIT/javascript/home/lesson13-14/js/script.js
@@ -1,99 +1,145 @@
 'use strict'
 
 $(function() {
-	var questions = [ {
+	var questions = [ 
+    {
   		question: 'What is ECMAScript?',
-  		answer: ['New programming language',
-  				'Recycled realization JavaScript',
-  				'Language specification JavaScript'],
-  		correctAnswer: {
-  			2: true
-  		}	
-	},
-	{
- 		question: 'What is JavaScript?',
- 		answer: ['Analog of Java with more functions',
-  				'High-level interpreted programming language',
-  				'Imperative programming language'],
- 		correctAnswer: {
-  			1: true
-  		}	
-	},
+  		answers: [ {
+        answer: 'New programming language',
+        right: false
+      },
+      {
+        answer: 'Recycled realization JavaScript',
+        right: false
+      },
+      {
+        answer: 'Language specification JavaScript',
+        right: true
+      }]
+    },
 
-	{
+	  {  
+ 		  question: 'What is JavaScript?',
+ 		  answers: [ {
+        answer:'Analog of Java with more functions',
+        right: false
+      },
+      {
+        answer: 'High-level interpreted programming language',
+        right: true
+      },
+      {
+        answer: 'Imperative programming language',
+        right: false
+      }]
+    },  			
+  				
+	  {
   		question: 'What constructions for cycles is in JavaScript?',
-  		answer: ['Only two: for и while.',
-  				'Only one: for',
-  				'Three: for, while и do...while.'],
- 		correctAnswer: {
-  			2: true
-  		}	
-	}];
+  		answers: [ {
+        answer: 'Only two: for и while.',
+        right: false
+      },
+      {
+        answer: 'Only one: for',
+        right: false
+      },
+      {
+        answer: 'Three: for, while и do...while.',
+        right: true
+      }]
+    }
+	];
 
 
 	var sendTolocal = JSON.stringify( questions );
-    var localTest = localStorage.setItem( "data", sendTolocal );
+    var localTest = localStorage.setItem( "questions", sendTolocal );
 
-    var restore = localStorage.getItem( "data" );
+    var restore = localStorage.getItem( "questions" );
     var actualTest = JSON.parse( restore ); 
 
     var html = $('#testing').html();
-	var content = tmpl(html, {
+	 var content = tmpl(html, {
 		data: actualTest
 		});
 
     $('.js-test').append(content);
 
+  var i, j;
+  var $inputs = $('input:checkbox');
 
 function checkAnswer(e) {
     
 	e.preventDefault();
     
-    var error = false;
-    
-    var user = [];
-    
-    for (var i = 0; i < actualTest.length; i++) {
-        
-   		var inputs = $('.box'+ i + 'input:checkbox');
-   		   		
-    	var userAnswered = {};
-   			for (var k = 0; k < inputs.length; k++) { 
-     		   var checked = inputs[k].checked;
-       
-       		 var right = actualTest[i].correctAnswer[k+1] == true;
-        
-       		 if (checked !== right) {
-       		 	userAnswered[k]=false;
-       		     error = true;
-                
-       		 } else {
-        		userAnswered[k]=true;
-        		};
-		};
-        user.push(userAnswered);
-	};
+ var rightAnswers = [];
 
+  var getRightAnswers = function() {
+
+    for ( i = 0; i < actualTest.length; i++ ) {
+
+      var testAnswers = actualTest[i].answers;
+
+      for (j = 0; j < testAnswers.length; j++) {
+
+        var currentAnswer = actualTest[i].answers[j].right;
+        rightAnswers.push(currentAnswer);
+
+      }
+    }
+  };
+
+  var givenAnswers = [];
+
+  var getGivenAnswers = function() {
+
+
+    $inputs.each(function () {
+
+      if ( $(this).prop('checked') ) {
+
+        givenAnswers.push(true);
+
+      } else {
+
+        givenAnswers.push(false);
+
+      }
+    });
+  };
+
+  var answered = 0;
+
+  var check = function () {
+
+    for (var i = 0; i < rightAnswers.length; i++) {
+
+      if ( rightAnswers[i] === true ) {
+
+        if ( rightAnswers[i] === givenAnswers[i] ) {
+          answered++;
+        }
+
+      }
+    }
+  };
+
+ getRightAnswers();
+  console.log('rightAnswers= ', rightAnswers);
+
+  getGivenAnswers();
+  console.log('givenAnswers= ', givenAnswers);
+
+  check();
+  console.log('answered= ', answered);
 
 	function modalWindow () {
 
-		for (var i = 0; i < actualTest.length; i++) {
-    			var inputs = $('.box' + i +' input:checkbox');
-    			
-    			for (var k = 0; k < actualTest[i].answer.length; k++) {
-    				
-    				var checked = inputs[k].checked;
-
-		if ((checked == true)) {
-			if ((user[i][k]) == true) {
+			if (check) {
 				var $modal = ('<div class="myModal"><div class="mymodal-inner"><h1 class="text-center">All correct!</h1><button class="center-block btn btn-primary btn-lg" id="exit">Exit</button></div></div>');
 			} else {
 				var $modal = ('<div class="myModal"><div class="mymodal-inner"><h1 class="text-center">Your answers is incorrect</h1><button class="center-block btn btn-primary btn-lg" id="exit">Exit</button></div></div>');
 			}
-		}
-		console.log(();
-	}
-	}
 
 		$('body').append($modal);
 
