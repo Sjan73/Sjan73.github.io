4df56fb9f0c167b26d4fe15864947628d06a299b
Andrii Palas <sajn73@gmail.com>
2016-02-29 21:39:59 +0200

    MVC

--- GoIT/javascript/home/lesson23-24/js/main.js
+++ GoIT/javascript/home/lesson23-24/js/main.js
@@ -1,6 +1,6 @@
 requirejs.config({
 	paths: {
-		'jquery': "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery"
+		'jquery': "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery"
 	},
 	shim: {
 		'jquery': {
